e0be818886a3ff16cdf65973dc54c3eed1e592ac
theanh2906 <theanh2906@outlook.com>
2022-11-23 00:44:28 +0700

    update layout

--- frontend/src/main/web/tailwind.config.js
+++ frontend/src/main/web/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
+    screens: {
+      tablet: '768px',
+      desktop: '1024px',
+      mobile: '320px',
+    },
   },
   plugins: [],
 };
